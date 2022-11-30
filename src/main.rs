9cb9792eb4cdd53448600530a465771966657cc8
William <williamlsh@protonmail.com>
2022-11-30 12:43:52 +0000

    Add config

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,45 @@
-use clap::StructOpt;
-use pigeon::{
-    archive::archive,
-    cli::{Cli, Commands},
-    export::export,
-    poll::poll,
-    sync::sync,
-};
+use clap::{Parser, Subcommand};
+use pigeon::Config;
+use std::path::PathBuf;
+use tokio::{fs::File, io::AsyncReadExt};
 
-fn main() {
+#[derive(Parser, Debug)]
+struct Cli {
+    /// Config file path
+    #[arg(short, long, value_name = "config.toml")]
+    config_path: PathBuf,
+
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    /// Poll Twitter users' timeline
+    Poll,
+    /// Push timeline to Telegram channel(s)
+    Push,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     env_logger::init();
 
     let cli = Cli::parse();
+    let mut config = load_config(cli.config_path).await?;
+
     match cli.command {
-        Commands::Archive(args) => archive(args),
-        Commands::Sync(args) => sync(args),
-        Commands::Export(args) => export(args),
-        Commands::Poll(args) => poll(args),
+        Command::Poll => {}
+        Command::Push => {}
     }
+    Ok(())
+}
+
+async fn load_config(path: PathBuf) -> anyhow::Result<Config> {
+    let mut file = File::open(path).await?;
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf).await?;
+
+    let config: Config = toml::from_slice(&buf)?;
+    Ok(config)
 }
